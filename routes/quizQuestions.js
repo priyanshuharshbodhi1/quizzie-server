049aaac5b2094f656fb0f2b908f94fc9674892a9
priyanshu <priyanshuqpwp@gmail.com>
2023-10-27 10:35:20 +0530

    added quiz deleting functionallity in quizQuestions.js

--- routes/quizQuestions.js
+++ routes/quizQuestions.js
@@ -19,4 +19,19 @@ router.get("/:quizId", async (req, res) => {
   }
 });
 
+router.delete("/:quizId", async (req, res) => {
+  try {
+    const quizId = req.params.quizId;
+    const quiz = await Quiz.findByIdAndDelete(quizId);
+
+    if (!quiz) {
+      return res.status(404).json({ message: "Quiz not found" });
+    }
+
+    res.json({ message: "Quiz deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred", error: error.message });
+  }
+});
+
 module.exports = router;
